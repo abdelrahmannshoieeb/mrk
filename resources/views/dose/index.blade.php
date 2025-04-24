1143f23570116ea63fbad752f25e98d91472913d
shoieb <abdoshoieb93@email.com>
2025-04-25 00:20:13 +0200

    contact

--- resources/views/dose/index.blade.php
+++ resources/views/dose/index.blade.php
@@ -56,6 +56,57 @@
         </figure>
     </section>
 
+    <section class="service-section relative overflow-hidden pb-14 pt-14 md:pb-16 md:pt-16 lg:pb-[88px] lg:pt-[88px] xl:pb-[100px] xl:pt-[100px]">
+   
+  
+        <article  class="flex flex-row flex-nowrap w-fit gap-9 pl-[7%] md:pl-[20%] md:pr-10 service-wrapper overflow-x-hidden reveal-me">
+          <!-- Testimonial Card 1 -->
+          <div class="w-[395px] md:w-[570px]">
+            <a href="https://www.facebook.com/share/p/1f6MwLdnb1/" target="_blank">
+              <img src="images/thumb1.jpg" alt="">
+            </a>
+            <div class="pl-16 -mt-3">
+            </div>
+          </div>
+          <div class="w-[395px] md:w-[570px]">
+            <a href="https://www.facebook.com/share/p/16ArrmSjpt/" target="_blank">
+              <img src="images/thumb2.jpg" alt="">
+            </a>
+            <div class="pl-16 -mt-3">
+            </div>
+          </div>
+          <div class="w-[395px] md:w-[570px]">
+            <a href="https://www.facebook.com/share/p/18jMmj4sa6/" target="_blank">
+              <img src="images/thumb3.jpg" alt="">
+            </a>
+            <div class="pl-16 -mt-3">
+            </div>
+          </div>
+          <div class="w-[395px] md:w-[570px]">
+            <a href="https://www.facebook.com/share/p/169YwLdnb1/" target="_blank">
+              <img src="images/thumb4.jpg" alt="">
+            </a>
+            <div class="pl-16 -mt-3">
+            </div>
+          </div>
+          <div class="w-[395px] md:w-[570px]">
+            <a href="https://www.facebook.com/share/p/1AGgeQ4W2i/" target="_blank">
+              <img src="images/thumb5.jpg" alt="">
+            </a>
+            <div class="pl-16 -mt-3">
+            </div>
+          </div>
+          <div class="w-[395px] md:w-[570px]">
+            <a href="https://www.facebook.com/share/p/1HSYozzpvd/" target="_blank">
+              <img src="images/thumb6.jpg" alt="">
+            </a>
+            <div class="pl-16 -mt-3">
+            </div>
+          </div>
+      
+        
+        </article>
+      </section>
 
     <!--==================================
 About Description Section Area
@@ -1007,58 +1058,5 @@ CTA Section
         </div>
         
     </section>
- 
-<section class="service-section relative overflow-hidden pb-14 pt-14 md:pb-16 md:pt-16 lg:pb-[88px] lg:pt-[88px] xl:pb-[100px] xl:pt-[100px]">
-   
-  
-    <article  class="flex flex-row flex-nowrap w-fit gap-9 pl-[7%] md:pl-[20%] md:pr-10 service-wrapper overflow-x-hidden reveal-me">
-      <!-- Testimonial Card 1 -->
-      <div class="w-[395px] md:w-[570px]">
-        <a href="https://www.facebook.com/share/p/1f6MwLdnb1/" target="_blank">
-          <img src="images/thumb1.jpg" alt="">
-        </a>
-        <div class="pl-16 -mt-3">
-        </div>
-      </div>
-      <div class="w-[395px] md:w-[570px]">
-        <a href="https://www.facebook.com/share/p/16ArrmSjpt/" target="_blank">
-          <img src="images/thumb2.jpg" alt="">
-        </a>
-        <div class="pl-16 -mt-3">
-        </div>
-      </div>
-      <div class="w-[395px] md:w-[570px]">
-        <a href="https://www.facebook.com/share/p/18jMmj4sa6/" target="_blank">
-          <img src="images/thumb3.jpg" alt="">
-        </a>
-        <div class="pl-16 -mt-3">
-        </div>
-      </div>
-      <div class="w-[395px] md:w-[570px]">
-        <a href="https://www.facebook.com/share/p/169YwLdnb1/" target="_blank">
-          <img src="images/thumb4.jpg" alt="">
-        </a>
-        <div class="pl-16 -mt-3">
-        </div>
-      </div>
-      <div class="w-[395px] md:w-[570px]">
-        <a href="https://www.facebook.com/share/p/1AGgeQ4W2i/" target="_blank">
-          <img src="images/thumb5.jpg" alt="">
-        </a>
-        <div class="pl-16 -mt-3">
-        </div>
-      </div>
-      <div class="w-[395px] md:w-[570px]">
-        <a href="https://www.facebook.com/share/p/1HSYozzpvd/" target="_blank">
-          <img src="images/thumb6.jpg" alt="">
-        </a>
-        <div class="pl-16 -mt-3">
-        </div>
-      </div>
-  
-    
-    </article>
-  </section>
-  
 </main>
 @endsection
